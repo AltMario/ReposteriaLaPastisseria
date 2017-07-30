6c476705a40c0af8fd107607e7930e51ea6c32f5
DevMario <mariodrush@gmail.com>
2017-07-30 19:50:35 +0200

    Se Implemento en tiempo real las Graficas de los Productos

--- CapaNegocios/ClaseInforme.cs
+++ CapaNegocios/ClaseInforme.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 
 namespace CapaNegocios
 {
     public class ClaseInforme:CapaDatos.ClaseABD
     {
+        public DataTable traerRol()
+        {
+            string sql = "select   nombre , cant from producto";
+            return TraerDatos(sql);
+        }
     }
 }
